9cc0e2fbbef752ece528029fcc9ddca5ff7dd8f2
Prashantgokak <prashantgokak500@gmail.com>
2026-01-11 17:55:07 +0530

    Update and rename bank.py to student.py

---
+++ student.py
@@ -0,0 +1,57 @@
+import os
+  
+def show_grade_criteria():
+    print("--- Grade Criteria ---")
+    print("90 - 100 : Grade S")
+    print("80 - 89  : Grade A")
+    print("65 - 79  : Grade B")
+    print("50 - 64  : Grade C")
+    print("40 - 49  : Grade D")
+    print("Below 40 : Grade F")
+    print("----------------------\n")
+
+
+def calculate_grade(avg):
+    if avg >= 90:
+        return "S"
+    elif avg >= 80:
+        return "A"
+    elif avg >= 65:
+        return "B"
+    elif avg >= 50:
+        return "C"
+    elif avg >= 40:
+        return "D"
+    else:
+        return "F"
+
+
+def main():
+    # Read Jenkins parameters
+    name = os.getenv("name", "jayaraj")
+    dept = os.getenv("dept", "BCA")
+    sem = os.getenv("sem", "3")
+
+    m1 = int(os.getenv("m1", 97))
+    m2 = int(os.getenv("m2", 84))
+    m3 = int(os.getenv("m3", 79))
+
+    show_grade_criteria()
+
+    print("--- Student Details ---")
+    print(f"Name: {name}")
+    print(f"Department: {dept}")
+    print(f"Semester: {sem}\n")
+
+    print("--- Subject Marks ---")
+    print(f"Subject 1: {m1}")
+    print(f"Subject 2: {m2}")
+    print(f"Subject 3: {m3}\n")
+
+    avg = (m1 + m2 + m3) / 3
+    print(f"Average Marks: {avg}")
+    print(f"Final Grade: {calculate_grade(avg)}")
+
+
+if __name__ == "__main__":
+    main()
